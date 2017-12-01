a031a408fa9bd68440d93673cc64fd9ba5220c88
Arthur Fabre <arthur@arthurfabre.com>
2017-12-01 18:04:57 +0100

    01: Use phony rule for getting input
    Replace $(INPUT) shenanigans with a PHONY match all pattern rule for reading
    the input.

--- 01.mk
+++ 01.mk
@@ -1,17 +1,16 @@
-# Reads from $(INPUT), prints result out
 include common.mk
-
-# Convert input to list of digits
-DIGITS:=$(call digits,$(INPUT))
-
-# Copy first number to back to make it circular
-DIGITS+=$(call car,$(DIGITS))
 
 # List of all the numbers that are followed by the same one
 LIST_NEXT=$(strip $(call LIST_NEXT_REC,$1,))
 LIST_NEXT_REC=$(if $(call eq,1,$(words $1)),$2,$(call LIST_NEXT_REC,$(call cdr,$1),$2 $(if $(call eq,$(call car,$1),$(call cadr,$1)),$(call car,$1))))
 
+.PHONY:
+%:
+	@# Convert input to list of digits
+	$(eval DIGITS:=$(call digits,$@))
 
+	@# Copy first number to back to make it circular
+	$(eval DIGITS+=$(call car,$(DIGITS)))
 
-DUPLICATES:=$(call LIST_NEXT,$(DIGITS))
-$(info DUPS=$(DUPLICATES))
+	$(eval DUPLICATES:=$(call LIST_NEXT,$(DIGITS)))
+	$(info DUPS=$(DUPLICATES))
