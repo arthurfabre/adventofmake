f66747f2828ba5bde552e37a54dea622402e4782
Arthur Fabre <arthur@arthurfabre.com>
2017-12-01 17:37:13 +0100

    1: Simplify digits(), add eq()
    Simplify digits() - I don't know why I thought all the lists needed to
    be packed and unpacked all the time.
    
    Add string equality function, eq().

--- common.mk
+++ common.mk
@@ -11,9 +11,15 @@ space:=$(empty) $(empty)
 digit_list:=0 1 2 3 4 5 6 7 8 9
 
 
+# STRING
+
+# If $1 is in $2, and $2 is in $1 then $1 == $2
+eq=$(and $(findstring $(strip $1),$(strip $2)),$(findstring $(strip $2),$(strip $1)))
+
+
 # LISTS
 
-# Pack a list into a single string. Useful for passing to functions
+# Pack a list into a single string. Useful for nested lists.
 pack=$(subst $(space),|,$(strip $1))
 
 # Unpack a string produced by pack()
@@ -33,7 +39,8 @@ p_cdr=$(call pack,$(call cdr,$(call unpack,$1)))
 # NUMBERS
 
 # Digits of number as list 1212 -> 1 2 1 2
-digits=$(call unpack,$(call _digits_rec,$(call pack,$(digit_list)),$(call pack,$1)))
+# Strip to remove last trailing space
+digits=$(strip $(call _digits_rec,$(digit_list),$1))
 # 1: digits left to process
 # 2: string to process
-_digits_rec=$(if $(strip $1),$(call _digits_rec,$(call p_cdr,$1),$(call pack,$(subst $(call p_car,$1),$(call p_car,$1)$(space),$2))),$2)
+_digits_rec=$(if $(strip $1),$(call _digits_rec,$(call cdr,$1),$(subst $(call car,$1),$(call car,$1)$(space),$2)),$2)
